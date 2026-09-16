57a92744aac2214f546fcb6304d4f4f2b3af4039
Taha Imran <tahaimran@arizona.edu>
2026-09-16 10:20:07 -0700

    Part 5: add model and index view tests, filter future questions

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question, Choice
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
