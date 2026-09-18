6efd5ddbee2190eceeab124d2d0c243740561530
Taha Imran <tahaimran@arizona.edu>
2026-09-18 14:58:08 -0700

    Part 7 complete: customize admin with inlines, list display, filters, search

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
